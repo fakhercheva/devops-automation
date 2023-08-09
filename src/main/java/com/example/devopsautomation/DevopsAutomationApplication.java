35ddd087f39a3798ebb4e46de34edf423692819e
fakher mekki <f.mekki@koous-it.com>
2023-08-09 15:26:14 +0200

    first commit

---
+++ src/main/java/com/example/devopsautomation/DevopsAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsAutomationApplication.class, args);
+    }
+
+}
